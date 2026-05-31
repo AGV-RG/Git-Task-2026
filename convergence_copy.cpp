3d3840b3fdd294d4f00607cf7991de1d4c767f30
saswatskar <saswatskar@gmail.com>
2026-05-31 17:08:32 +0530

    added file

---
+++ convergence_copy.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int t , n , left , right ,*a; 
+
+    std::cin>>t;
+    for(int j = 0 ; j<t ; j++)
+    {
+        std::cin>>n;
+        a = new int[n];
+        left = 0 ;
+        right = 0;
+
+        for(int i = 0 ; i< n ; i++)
+        {
+            std::cin>>a[i];
+        }
+        std::sort(a , a+n);
+        //median = a[n/2]
+        for(int i =  0 ; i < n ; i++)
+        {
+            if(a[i]==a[n/2])continue;
+            if(a[i] > a[n/2])right++;
+            else left++;
+        }
+        if(left>right) std::cout<<left<<"\n";
+        else std::cout<<right<<"\n";
+        delete[] a;
+    }
+
+
+    return 0;
+}
